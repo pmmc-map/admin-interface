007bbe0108ef791ca6f1c036a3afdcb6d3581d4f
Jas Delgado <jas@jasdelgado.com>
2019-12-06 15:46:13 -0800

    make email field a real form

--- src/Dashboard/DataDownloadForm.js
+++ src/Dashboard/DataDownloadForm.js
@@ -6,9 +6,11 @@ const DataDownloadForm = () => {
 	const [successText, setSuccessText] = useState('');
 	const [errorText, setErrorText] = useState('');
 
-	const onSubmitEmail = () => {
+	const onSubmitEmail = e => {
+		e.preventDefault();
 		setErrorText('');
 		setSuccessText('');
+
 		const emailRe = /[\S]+([@])(\S)+(\.)\S+$/;
 		const makeEmailRequest = async () => {
 			const emailResponse = sendEmail(email);
@@ -32,27 +34,31 @@ const DataDownloadForm = () => {
 				We will send a .csv file with all of the collected app data to
 				your email address.
 			</p>
-			<div className='field is-grouped'>
-				<p className='control'>
-					<input
-						className={`input${errorText ? ' is-danger' : ''}`}
-						type='email'
-						placeholder='email@email.com'
-						onChange={e => setEmail(e.target.value.trim())}
-					/>
-				</p>
-				<p className='control'>
-					<button className='button is-info' onClick={onSubmitEmail}>
-						Submit
-					</button>
-				</p>
-				{errorText ? (
-					<p className='has-text-danger'>{errorText}</p>
-				) : null}
-				{successText ? (
-					<p className='has-text-success'>{successText}</p>
-				) : null}
-			</div>
+			<form onSubmit={onSubmitEmail}>
+				<div className='field is-grouped'>
+					<p className='control'>
+						<input
+							className={`input${errorText ? ' is-danger' : ''}`}
+							type='email'
+							placeholder='email@email.com'
+							onChange={e => setEmail(e.target.value.trim())}
+						/>
+					</p>
+					<p className='control'>
+						<button
+							className='button is-info'
+							type='submit'
+							onClick={onSubmitEmail}
+						>
+							Submit
+						</button>
+					</p>
+				</div>
+			</form>
+			{errorText ? <p className='has-text-danger'>{errorText}</p> : null}
+			{successText ? (
+				<p className='has-text-success'>{successText}</p>
+			) : null}
 		</div>
 	);
 };
